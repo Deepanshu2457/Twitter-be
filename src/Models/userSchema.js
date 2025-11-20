0b7f4deb287c7af54771da4f0c0b8d121c3529e8
deepanshu <deepanshusharma981098@gmail.com>
2025-11-20 23:25:58 +0530

    update

--- src/Models/userSchema.js
+++ src/Models/userSchema.js
@@ -2,12 +2,26 @@ const mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema({
     firstname : {
-
+        type:String,
+        required : true,
+        maxlength : 20,
+        minlength : 2,
+        trim: true
     },
     lastname : {
+           type:String,
+        required : true,
+        maxlength : 20,
+        minlength : 2,
+        trim: true
 
     },
     username : {
+           type:String,
+        required : true,
+        maxlength : 20,
+        minlength : 2,
+        trim : true
 
     },
     mail : {
