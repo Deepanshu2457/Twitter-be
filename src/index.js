2a34623d80509c1f98e5b643c1080672f40a840b
deepanshu <deepanshusharma981098@gmail.com>
2025-12-01 11:07:23 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 require("dotenv").config()
 const express=require("express")
 const {authRouter} = require("./Routes/AuthRoute")
+const { commentRouter } = require("./Routes/Comment.Route")
+const { otpRoutes } = require("./Routes/Otp.routes")
+const { postRouter } = require("./Routes/PostRoutes")
+const { ProfileRoutes } = require("./Routes/ProfileRoutes")
+const { replyRoute } =require("./Routes/ReplyRoute")
+
 
 const app=express()
 app.use(express.json())
@@ -23,6 +29,12 @@ app.get("/",(req,res) => {
     res.send("Home Page")
 })
 app.use("/api",authRouter)
+app.use("/api",postRouter)
+app.use("/api",commentRouter)
+app.use("/api",otpRoutes)
+app.use("/api",postRouter)
+app.use("/api", ProfileRoutes)
+app.use("/api",replyRoute)
 
 
 
